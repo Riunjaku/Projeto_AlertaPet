deee42812f75ee3a98482ac01151209620f8f334
Eduardo Felipe <eduardo.felipe.s.miranda@gmail.com>
2017-10-17 22:21:11 -0200

    Mapa otimizado

--- app/src/main/java/com/aniharu/alertapet/MainActivity.java
+++ app/src/main/java/com/aniharu/alertapet/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Set the fragment initially
+        //Coloca o fragmento inicial
         MainFragment fragment = new MainFragment();
         android.support.v4.app.FragmentTransaction fragmentTransaction =
                 getSupportFragmentManager().beginTransaction();
@@ -36,21 +36,23 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    //Verifica o item selecionado no menu e realiza e chama o fragmento correspondente
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
         // cuida do menu de navegação trocando os fragmentos na activity
 
-        /*int id = item.getItemId();
+        int id = item.getItemId();
 
-        if (id == R.id.nav_perfil) {
+       /* if (id == R.id.nav_perfil) {
 
             PerfilFragment fragment = new PerfilFragment();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -59,7 +61,7 @@ public class MainActivity extends AppCompatActivity
             fragmentTransaction.commit();
 
 
-        } else if (id == R.id.nav_map) {
+        } else */if (id == R.id.nav_map) {
 
             MainFragment fragment = new MainFragment();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -67,7 +69,7 @@ public class MainActivity extends AppCompatActivity
             fragmentTransaction.replace(R.id.fragment_container, fragment);
             fragmentTransaction.commit();
 
-        } else if (id == R.id.nav_lista_animais) {
+        }/* else if (id == R.id.nav_lista_animais) {
 
             ListaAnimaisFragment fragment = new ListaAnimaisFragment();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -75,7 +77,7 @@ public class MainActivity extends AppCompatActivity
             fragmentTransaction.replace(R.id.fragment_container, fragment);
             fragmentTransaction.commit();
 
-        } else if (id == R.id.nav_registrar_animais) {
+        } */ else if (id == R.id.nav_registrar_animais) {
 
             RegistrarAnimaisFragment fragment = new RegistrarAnimaisFragment();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -83,7 +85,7 @@ public class MainActivity extends AppCompatActivity
             fragmentTransaction.replace(R.id.fragment_container, fragment);
             fragmentTransaction.commit();
 
-        } else if (id == R.id.nav_chat) {
+        } /*else if (id == R.id.nav_chat) {
 
             BatePapoFragment fragment = new BatePapoFragment();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -109,4 +111,5 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
--- app/src/main/java/com/aniharu/alertapet/MainFragment.java
+++ app/src/main/java/com/aniharu/alertapet/MainFragment.java
@@ -1,14 +1,187 @@
 package com.aniharu.alertapet;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener,
+        GoogleMap.OnMarkerClickListener,
+        View.OnClickListener
+{
+
+        GoogleMap mGoogleMap;
+        MapView mMapView;
+        GoogleApiClient mGoogleApiClient;
+        private static final String TAG = "fudeu";
+        private double longitude;
+        private double latitude;
+        SupportMapFragment mapFragment;
 
 
-    public MainFragment() {
-        // Required empty public constructor
-    }
+        public MainFragment() {
 
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            if (mapFragment == null) {
+                mapFragment = SupportMapFragment.newInstance();
+                mapFragment.getMapAsync(this);
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.map, mapFragment)
+                        .commit();
+            }
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            mMapView = (MapView) view.findViewById(R.id.map);
+            mMapView.onCreate(savedInstanceState);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+
+            mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                    .enableAutoManage(getActivity() /* FragmentActivity */,
+                            this /* OnConnectionFailedListener */)
+                    .addConnectionCallbacks(this)
+                    .addApi(LocationServices.API)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(Places.GEO_DATA_API)
+                    .build();
+
+
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            if (mGoogleApiClient != null) {
+                mGoogleApiClient.connect();
+            }
+            mMapView.onStart();
+
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.stopAutoManage((getActivity()));
+                mGoogleApiClient.disconnect();
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            mMapView.onResume();
+            mGoogleApiClient.reconnect();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent result) {
+
+            super.onActivityResult(requestCode, resultCode, result);
+
+            mGoogleApiClient.connect();
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.v(TAG, "view click event");
+        }
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+
+
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+            Log.d("Dominando", "onDisconnected");
+        }
+
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            Toast.makeText(getActivity(), "onMarkerClick", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            this.mGoogleMap = googleMap;
+            //desativa os controles do mapa
+            mGoogleMap.getUiSettings().setZoomControlsEnabled(false);
+            mGoogleMap.setBuildingsEnabled(false);
+            mGoogleMap.getUiSettings().setZoomGesturesEnabled(false);
+            //seta o rio de janeiro como ponto inicial
+            LatLng RIODEJANEIRO = new LatLng(-22.927350, -43.422644);
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(RIODEJANEIRO, 12));
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            //seta as bordas do rio de janeiro
+            LatLngBounds RIODEJANEIROBOUNDS = new LatLngBounds(
+                    new LatLng(-23.056930, -43.790686), new LatLng(-22.745730, -43.102667));
+            mGoogleMap.setLatLngBoundsForCameraTarget(RIODEJANEIROBOUNDS);
+
+
+        }
+
+        @Override
+        public void onMapLongClick(LatLng latLng) {
+
+        }
+
+        @Override
+        public void onMarkerDragStart(Marker marker) {
+
+        }
+
+        @Override
+        public void onMarkerDrag(Marker marker) {
+
+        }
+
+        @Override
+        public void onMarkerDragEnd(Marker marker) {
+
+        }
 
 }
